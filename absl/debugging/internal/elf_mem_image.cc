2c5e96b14ba38203d8244c62e7b60a519bd77a1f
Showta Ishizaki <zakinko@snowrabbit.org>
2026-09-06 22:03:19 +0900

    Add NetBSD and DragonFly BSD fixes carried by pkgsrc and DPorts
    Gathered from what the BSD ports trees patch and rebased onto master.
    NetBSD: elf_mem_image.cc (DT_GNU_HASH guard, Versym keyed on
    _SYS_EXEC_ELF_H_ >= 2 for -current), sysinfo.cc GetTID cast (#1518),
    unscaledcycleclock.cc powerpc Frequency(), cctz _XOPEN_SOURCE.
    DragonFly: config.h HAVE_MMAP/HAVE_PTHREAD_GETSCHEDPARAM, sysinfo.cc
    via the FreeBSD branches, vdso_support.cc Elf_Auxinfo aliases, cctz
    _XOPEN_SOURCE and the lookup test, in the form DPorts carries.
    
    Not yet built on any BSD from this tree; that is the next step.

--- absl/debugging/internal/elf_mem_image.cc
+++ absl/debugging/internal/elf_mem_image.cc
@@ -225,9 +225,11 @@ void ElfMemImage::Init(const void *base) {
       case DT_HASH:
         sysv_hash = reinterpret_cast<uint32_t *>(value);
         break;
+#ifdef DT_GNU_HASH
       case DT_GNU_HASH:
         gnu_hash = reinterpret_cast<uint32_t *>(value);
         break;
+#endif
       case DT_SYMTAB:
         dynsym_ = reinterpret_cast<ElfW(Sym) *>(value);
         break;
@@ -377,7 +379,7 @@ void ElfMemImage::SymbolIterator::Update(uint32_t increment) {
   const ElfW(Versym) *version_symbol = image->GetVersym(index_);
   ABSL_RAW_CHECK(symbol && version_symbol, "");
   const char *const symbol_name = image->GetDynstr(symbol->st_name);
-#if defined(__NetBSD__)
+#if defined(__NetBSD__) && ((_SYS_EXEC_ELF_H_ + 0) < 2)
   const int version_index = version_symbol->vs_vers & VERSYM_VERSION;
 #else
   const ElfW(Versym) version_index = version_symbol[0] & VERSYM_VERSION;
